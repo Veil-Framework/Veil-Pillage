ecb72e7c8e479c172fe6b4c70e43705c79f77192
HarmJ0y <will@harmj0y.net>
2015-01-07 14:04:52 -0500

    Bug fix for management/enable_proxy

--- modules/management/enable_proxy.py
+++ modules/management/enable_proxy.py
@@ -57,6 +57,7 @@ class Module:
         proxyEnableCmd = "reg add \"HKCU\\Software\\Microsoft\\Windows\\CurrentVersion\\Internet Settings\" /v ProxyEnable /t REG_DWORD /d 1 /f"
 
         proxySetCmd = "reg add \"HKCU\\Software\\Microsoft\\Windows\\CurrentVersion\\Internet Settings\" /v ProxyServer /t REG_SZ /d %s /f" % (proxyUrl)
+        proxy = ""
 
         for target in self.targets:
 
